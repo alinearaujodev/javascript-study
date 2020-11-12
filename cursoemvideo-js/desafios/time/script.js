81854589462dc7c8121b79e782d31fb5c2b3d343
Aline Araújo <alineprofissional@outlook.com>
2020-11-12 16:16:30 -0300

    finalizado desafio time

--- cursoemvideo-js/desafios/time/script.js
+++ cursoemvideo-js/desafios/time/script.js
@@ -1,18 +1,28 @@
-var now = new Date()
-var hour = now.getHours()
-var min = now.getMinutes()
-var time = document.getElementById('time')
-var box = document.getElementById('box')
-var corpo = document.getElementById('body')
-
-if (hour < 12){
-    corpo.style.backgroundImage = 'https://image.freepik.com/fotos-gratis/vista-fantastica-do-nascer-do-sol-sobre-as-nuvens-vistas-da-janela-do-aviao_76000-2233.jpg'
-    box.style.backgroundColor = '#a3b40b'
-    time.innerText = `Good Morning! It's ${hour}:${min}`
-} else if (hour < 18){
-    box.style.backgroundColor = '#aa521f'
-    time.innerText = `Good Afternoon! It's ${hour}:${min}`
-} else {
-    box.style.backgroundColor = '#060747'
-    time.innerText = `Good Everning! It's ${hour}:${min}`
+function londing (){
+    var now = new Date()
+    var hour = now.getHours()
+    var min = now.getMinutes()
+    var time = document.getElementById('time')
+    var box = document.getElementById('box')
+    var photo = document.getElementById('image')
+    
+    if (hour < 12){
+        document.body.style.backgroundColor = '#db7d34'
+        box.style.color = '#db7d34'
+        box.style.backgroundColor = 'white'
+        time.innerText = `Good Morning! It's ${hour}:${min}`
+        photo.src = 'img/bomdia.jpg'
+    } else if (hour < 18){
+        document.body.style.backgroundColor = '#e27059'
+        box.style.color = '#e27059'
+        box.style.backgroundColor = 'white'
+        time.innerText = `Good Afternoon! It's ${hour}:${min}`
+        photo.src = 'img/boatarde.jfif'
+    } else {
+        document.body.style.backgroundColor = '#0f1b2b'
+        box.style.color = '#0f1b2b'
+        box.style.backgroundColor = 'white'
+        time.innerText = `Good Everning! It's ${hour}:${min}`
+        photo.src = 'img/boanoite.jpg'
+    }
 }
