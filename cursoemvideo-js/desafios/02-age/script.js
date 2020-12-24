7dbe0f44ac7e37993f5f1faf07e9491cc476bac8
Aline <alineprofissional@outlook.com>
2020-12-24 18:23:02 -0300

    desafio age finalizado

--- cursoemvideo-js/desafios/02-age/script.js
+++ cursoemvideo-js/desafios/02-age/script.js
@@ -2,41 +2,44 @@ function verificar(){
     var now = new Date()
     var atual = now.getFullYear()
     var year = document.getElementById('year')
+    var res = document.querySelector('div#age')
 
-    if (year.value.length == 0 || year.value > atual){
+    // SABER SE ANO É VALIDO
+    if (year.value.length == 0 || Number(year.value) > atual){
         alert('Erro! Verifique os dados e tente novamente')
     } else {
-        var res = document.getElementById('age')
         var sexo = document.getElementsByName('sexo')
-        var idade = atual - Number(year.value) 
+        var idade = atual - Number(year.value)
         var genero = ''
-        var photo = document.getElementById('image')
+        var photo = document.createElement('img')
+        photo.setAttribute('id', 'foto')
 
+        // CONDIÇÃO PARA SEXO MASCULINO
         if (sexo[0].checked){
             genero = 'Homem'
             if (idade <=12){
-                photo.src = 'boy.jpg'
+                photo.setAttribute('src', 'boy.jpg')
             }  else if (idade > 12 && idade <=20){
-                photo.src = 'young-man.jpg'
+                photo.setAttribute('src', 'young-man.jpg')
             }  else if (idade > 20 && idade <=55){
-                photo.src = 'man.jpg'
+                photo.setAttribute('src', 'man.jpg')
             } else if (idade > 55){
-                photo.src = 'old-woman.jpg'
+                photo.setAttribute('src', 'old-man.jpg')
             } 
-        } else if (sexo[1].checked){
+        } else{
+            // CONDIÇÃO PARA SEXO FEMININO
             genero = 'Mulher'
             if (idade <= 12){
-                photo.src = 'girl.jpg'
+                photo.setAttribute('src', 'girl.jpg')
             } else if (idade > 12 && idade <=20){
-                photo.src = 'young-woman.jpg'
+                photo.setAttribute('src', 'young-woman.jpg')
             } else if (idade > 20 && idade <=55){
-                photo.src = 'woman.jpg'
+                photo.setAttribute('src', 'woman.jpg')
             } else if (idade > 55){
-                photo.src = 'old-woman.jpg'
-            }
+                photo.setAttribute('src', 'old-woman.jpg')
+            }           
         }
-
-        res.innerHTML = `Verificamos que você é ${genero} e tem ${idade} anos`
+        res.innerHTML = `Verificamos que você é ${genero} e tem ${idade} anos`  
         res.appendChild(photo)
     }
 
