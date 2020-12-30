b43f120286ce28147fa8c3a6071d324c00132674
Aline <alineprofissional@outlook.com>
2020-12-30 20:08:04 -0300

    finalizado teoria while e do while

--- cursoemvideo-js/aula10repeticao.js
+++ cursoemvideo-js/aula10repeticao.js
@@ -1,1 +1,13 @@
-console.log('Blz?')
+// REPETICAO PARA COMER UMA PIZZA DE OITO PEDAÇOS COM WHILE
+var c = 1
+while (c <= 8) {
+    console.log('Comeu fatia!')
+    c++
+}
+
+// REPETICAO PARA CONTAR DE 1 A 10 COM DO ... WHILE
+var i = 1
+do {
+    console.log(i)
+    i++
+} while (i <=10)
