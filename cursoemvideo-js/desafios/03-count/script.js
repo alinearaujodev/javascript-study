b8b5db7e893e8ea99a802dbbdd18040c5b6cf3fb
Aline <alineprofissional@outlook.com>
2021-01-02 21:27:29 -0300

    desafio count finalizado

--- cursoemvideo-js/desafios/03-count/script.js
+++ cursoemvideo-js/desafios/03-count/script.js
@@ -3,22 +3,35 @@ function contar(){
     var final = document.getElementById('fim')
     var passo = document.getElementById('passo')
     var res = document.getElementById('resultado')
-    var c = Number(comeco.value)
     
-    // validação dos campos - verificar se campos estao vazios
-    if (Number(comeco.value) == '' || Number(final.value) == ''){
-        res.innerText = 'Impossivel contar'
-    }
+    // validação dos campos
+    if (comeco.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
+        alert('Erro! Falta dados')
+    } else {
+        // Fazer a contagem
+        res.innerHTML = 'Contando... <br>'
+        var c = Number(comeco.value)
+        var f = Number(final.value)
+        var p = Number(passo.value)
 
-    // validação dos campos - verificar se campo passo é maior que 0
-    if (Number(passo.value) == 0){
-        alert('Passo invalido! Considerando PASSO 1')
+        // validar campo passo maior que 0
+        if (p <= 0){
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        
+        if (c > f) {
+            // contagem decrecente
+            for (var i = c; i>=f; i-=p){
+                res.innerHTML += `\u{1F449} ${i} `
+            }
+        } else {
+            // contagem crescente
+            for (var i = c; i<=f; i+=p){
+                res.innerHTML += `\u{1F449} ${i} `
+            }
+           
+        }
+        res.innerHTML += ` \u{1F3C1}`
     }
-
-    // Fazer a contagem
-    res.innerText = 'Contando... '
-    do {
-        res.innerText = `${c} `
-        c = c + Number(passo.value)
-    } while (c<=Number(final.value))
 }
