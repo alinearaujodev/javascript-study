d4fa58d499c55b73a8cb0c3d9251559d7df66e05
Aline <alineprofissional@outlook.com>
2021-01-03 16:09:24 -0300

    desafio tabuada finalizado

--- cursoemvideo-js/desafios/04-tabuada/script.js
+++ cursoemvideo-js/desafios/04-tabuada/script.js
@@ -1,9 +1,15 @@
 function calcular(){
     var num = document.getElementById('num')
     var res = document.getElementById('resultado')
-    var c
 
-    for (c=0; c<=num; c++){
-        res.innerHTML = `${num} X ${c} = ${num*c} `
+    // saber se campo esta vazio
+    if (num.value.length == 0){
+        alert('Erro! Campo vazio, digite um numero')
+    }
+    
+    // Fazer a tabuada
+    res.innerHTML = `A tabuada do <strong>${Number(num.value)}</strong>:<br>`
+    for (var c=0; c<=10; c++){
+        res.innerHTML += `${Number(num.value)} X ${c} = ${Number(num.value)*c} <br>`
     }
 }
