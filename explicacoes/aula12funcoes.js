ddeb01b8395e42751789196528a6cbf070259078
alinearaujodev <alineprofissional@outlook.com>
2021-10-14 17:31:12 -0300

    add folders

---
+++ explicacoes/aula12funcoes.js
@@ -0,0 +1,16 @@
+function num(n) {
+    if(n%2==0){
+      return 'par'
+    }else {
+      return 'impar'
+    }
+}
+
+  let res = num(12)
+  console.log(res)
+
+function soma (n1, n2){
+    return n1 + n2
+}
+
+console.log(soma(5, 2))
