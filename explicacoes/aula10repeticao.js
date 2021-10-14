021eab0a57b2a1c2b97dce635415028181a320b5
alinearaujodev <alineprofissional@outlook.com>
2021-10-14 12:50:34 -0300

    organização das pastas

---
+++ explicacoes/aula10repeticao.js
@@ -0,0 +1,18 @@
+// REPETICAO PARA COMER UMA PIZZA DE OITO PEDAÇOS COM WHILE
+var c = 1
+while (c <= 8) {
+    console.log('Comeu fatia!')
+    c++
+}
+
+// REPETICAO PARA CONTAR DE 1 A 10 COM DO ... WHILE
+var i = 1
+do {
+    console.log(i)
+    i++
+} while (i <=10)
+
+// REPETICAO PARA CONTAR DE 10 ATE 1 COM FOR 
+for(var a = 10; a >= 1; a--){
+    console.log(a)
+}
