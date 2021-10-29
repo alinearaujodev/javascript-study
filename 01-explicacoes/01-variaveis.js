63db28b978e0e349698d1e96b6c932dbd739cd4f
alinearaujodev <alineprofissional@outlook.com>
2021-10-29 18:51:11 -0300

    organização das pastas

---
+++ 01-explicacoes/01-variaveis.js
@@ -0,0 +1,22 @@
+// No JS não precisa de tipagem, portanto você somente precisa declarar o nome da variável e seu valor
+var numero = 1;
+
+// Tipo de Variaveis 
+var num = 5; // Number
+var vouF = false; // Boolean: Falso
+var Vouf = true; // Boolean: Verdadeiro
+var nome = 'Aline'; // String: Caracter 
+var nullVar = null; // Null: Variável sem valor
+var und; // Undefined: variável foi declarada, pórem não recebeu nenhum valor
+
+// Declarando as variáveis
+var variavel; // Aqui como não foi definido o valor, a variavel será tratada como undefined
+
+// Escopo global e local
+var global = 'global';
+console.log(global)
+
+function escopolocal(){
+    let local = 'local';
+    console.log(local)
+}
--- explicacoes/01-variaveis.js
+++
@@ -1,9 +0,0 @@
-// No JS não precisa de tipagem, portanto você somente precisa declarar o nome da variável e seu valor
-var numero = 1;
-
-// Tipo de Variaveis 
-var num = 5; // Number
-var vouF = false; // Boolean: Falso
-var Vouf = true; // Boolean: Verdadeiro
-var nome = 'Aline'; // String: Caracter 
-console.log(nome);
